29805b7a8462ac9e18527e0b8eb8e9761e638935
Ryne Emory <ryneemory@gmail.com>
2015-06-04 10:49:55 -0600

    finished median and mode

--- grader.js
+++ grader.js
@@ -1,22 +1,97 @@
 module.exports = {
   letterGrader: function(testScore) {
     var EquivalentLetterGrade;
-    if (testScore>=90) {
-      EquivalentLetterGrade= "A";
+    if (testScore>=98) {
+      EquivalentLetterGrade= "A+";
     }
-    else if (testScore >=80) {
+    else if (testScore >=93) {
+      EquivalentLetterGrade = "A";
+    }
+    else if (testScore >=90) {
+      EquivalentLetterGrade = "A-";
+    }
+    else if (testScore >=88) {
+      EquivalentLetterGrade = "B+";
+    }
+    else if (testScore >=83) {
       EquivalentLetterGrade = "B";
     }
-    else if (testScore >=70) {
+    else if (testScore >=80) {
+      EquivalentLetterGrade = "B-";
+    }
+    else if (testScore >=78) {
+      EquivalentLetterGrade = "C+";
+    }
+    else if (testScore >=73) {
       EquivalentLetterGrade = "C";
     }
-    else if (testScore >=60) {
+    else if (testScore >=70) {
+      EquivalentLetterGrade = "C-";
+    }
+    else if (testScore >=68) {
+      EquivalentLetterGrade = "D+";
+    }
+    else if (testScore >=63) {
       EquivalentLetterGrade = "D";
+    }
+    else if (testScore >=60) {
+      EquivalentLetterGrade = "D-";
     }
     else {
       EquivalentLetterGrade = "F";
     };
     return EquivalentLetterGrade;
-  }
+  },
+
+    // medianScore: function (arr) {
+    //   var median, i, i1, i2;
+    //   function isOdd(value) {
+    //   	if (value % 2 === 1) {
+    //   		return true;
+    //     }
+    //   	else {
+    //   		return false;
+    //     };
+    //   if (isOdd(arr.length) === true ) {
+    //       i = Math.round(arr.length / 2 );
+    //       return arr[i];
+    //   }
+    //   else {
+    //       i1 = arr.length / 2 ;
+    //       i2 = arr.length / 2 + 1 ;
+    //       median = (arr[i1] + arr[i2]) / 2;
+    //       return median;
+    //     }
+    // },
+
+    medianScore: function (arr) {
+
+        var half = Math.floor(arr.length/2);
+
+        if(arr.length % 2 !== 0)
+            return arr[half];
+        else
+            return (arr[half-1] + arr[half]) / 2.0;
+    },
+
+    modeScore: function (arr) {
+       var obj = {};
+       var max = 0;
+       for (i=0; i < arr.length; i++) {
+         if (obj[arr[i]]) {
+           obj[arr[i]] ++;
+           if ([arr[i]] > max) {
+             max = arr[i];
+           }
+         }
+         else {
+           obj[arr[i]] = [arr[i]=1];
+         }
+       }
+       return max;
+     }
+
+
+
 
 }
--- spec/spec.js
+++ spec/spec.js
@@ -2,10 +2,22 @@ var code = require('./../grader.js')
 
 describe('Grading Exercise', function() {
   it('takes a number grade and returns the equivalent letter value.', function () {
-    expect(code.letterGrader(78)).toEqual("C");
+    expect(code.letterGrader(78)).toEqual("C+");
     expect(code.letterGrader(84)).toEqual("B");
     expect(code.letterGrader(30)).toEqual("F");
     expect(code.letterGrader(93)).toEqual("A");
-    expect(code.letterGrader(60)).toEqual("D");
+    expect(code.letterGrader(60)).toEqual("D-");
+    expect(code.letterGrader(70)).toEqual("C-");
+    expect(code.letterGrader(80)).toEqual("B-");
+    expect(code.letterGrader(90)).toEqual("A-");
+    expect(code.letterGrader(100)).toEqual("A+");
+    expect(code.letterGrader(69)).toEqual("D+");
+  })
+  it('takes an array of grades and returns the median value', function () {
+    expect(code.medianScore([52,80,80,86,94])).toEqual(80);
+    expect(code.medianScore([3, 4, 5, 7,  9, 24, 45, 53])).toEqual(8);
+  })
+  it('takes an array of grades and returns the mode value', function () {
+    expect(code.modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92);
   })
 })
